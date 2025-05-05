4456d02f6c065893560f1f4046ac1c1e9f94e70d
Ashish <ashishiit9@gmail.com>
2025-05-05 14:48:57 +0530

    login api modify

--- SkillsEvalBackend/routes/userRouter.js
+++ SkillsEvalBackend/routes/userRouter.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import {registerUser,logoutUser} from "../controller/userController.js";
+import { registerUser, logoutUser, loginUser } from "../controller/userController.js";
 // import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middleware/auth-middleware.js";
 const router = Router()
 
 
 router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
 
 
 //secure routes
-router.route("/logout").post(verifyJwt,logoutUser)
+router.route("/logout").post(verifyJwt, logoutUser)
 
 export default router
 
